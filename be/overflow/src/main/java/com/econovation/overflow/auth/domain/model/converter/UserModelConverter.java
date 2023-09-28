3385cff09ee9b97a4bda39d8d6edb4a797c60cb9
kssumin <201566@jnu.ac.kr>
2023-09-29 01:16:44 +0900

    feat : SaveUserRequest를 UserModel로 변환하는 Converter 구현

---
+++ be/overflow/src/main/java/com/econovation/overflow/auth/domain/model/converter/UserModelConverter.java
@@ -0,0 +1,23 @@
+package com.econovation.overflow.auth.domain.model.converter;
+
+import com.econovation.overflow.auth.domain.model.UserModel;
+import com.econovation.overflow.auth.web.dto.request.SaveUserRequest;
+import com.econovation.overflow.common.support.converter.AbstractDtoConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserModelConverter implements AbstractDtoConverter<SaveUserRequest, UserModel> {
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public UserModel from(SaveUserRequest request) {
+		return UserModel.builder()
+				.nickname(request.getNickname())
+				.email(request.getEmail())
+				.password(passwordEncoder.encode(request.getPassword()))
+				.build();
+	}
+}
