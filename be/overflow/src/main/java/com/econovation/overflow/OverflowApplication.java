e205bc2ed9c9bcef207657ccdb199574f4f191dd
kssumin <201566@jnu.ac.kr>
2023-09-28 21:59:24 +0900

    feat : 프로젝트 환경 세팅

---
+++ be/overflow/src/main/java/com/econovation/overflow/OverflowApplication.java
@@ -0,0 +1,12 @@
+package com.econovation.overflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverflowApplication.class, args);
+	}
+}
