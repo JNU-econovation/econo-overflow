62e1c699a135dd56a68d5b7edc06cf187fc8c6fe
kssumin <201566@jnu.ac.kr>
2023-09-29 01:07:53 +0900

    feat : email 중복 확인 controller 구현

---
+++ be/overflow/src/main/java/com/econovation/overflow/auth/web/controller/ValidController.java
@@ -0,0 +1,28 @@
+package com.econovation.overflow.auth.web.controller;
+
+import com.econovation.overflow.auth.domain.usecase.ValidEmailUseCase;
+import com.econovation.overflow.auth.web.dto.request.ValidEmailRequest;
+import com.econovation.overflow.common.support.respnose.ApiResponse;
+import com.econovation.overflow.common.support.respnose.ApiResponseBody.SuccessBody;
+import com.econovation.overflow.common.support.respnose.ApiResponseGenerator;
+import com.econovation.overflow.common.support.respnose.MessageCode;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class ValidController {
+	private final ValidEmailUseCase validEmailUseCase;
+
+	@PostMapping("/email-check")
+	public ApiResponse<SuccessBody<Void>> validEmail(@RequestBody @Valid ValidEmailRequest request) {
+		validEmailUseCase.execute(request);
+		return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.CREATE);
+	}
+}
