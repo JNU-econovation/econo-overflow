2b038ee5f210124c1f3324ebc263d4cb48bf1613
Klomachenko <leekyumin0901@naver.com>
2023-10-03 15:44:43 +0900

    feat : BoardRead 구현 (스타일 제외)

--- fe/src/components/pages/BoardRead.jsx
+++ fe/src/components/pages/BoardRead.jsx
@@ -5,7 +5,19 @@ import './BoardRead.css';
 const BoardRead = () => {
   const [post, setPosts] = useState(mockPosts);
 
-  return <div>ㅎㅇ</div>;
+  return (
+    <div className='container'>
+      <div className='title'>
+        <p>{post[0].title}</p>
+      </div>
+      <div className='content'>
+        <p>{post[0].content}</p>
+      </div>
+      <div className='comment'>
+        <p>댓글창임여</p>
+      </div>
+    </div>
+  );
 };
 
 export default BoardRead;
