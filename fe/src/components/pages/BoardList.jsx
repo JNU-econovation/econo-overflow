1993cfec8c640df4647a8bc82b19552785d269f6
Klomachenko <leekyumin0901@naver.com>
2023-10-03 15:08:05 +0900

    feat : mockData 생성

--- fe/src/components/pages/BoardList.jsx
+++ fe/src/components/pages/BoardList.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 
 const BoardList = () => {
-  let [title, setTitle] = useState('');
-  let [content, setContent] = useState('');
+  let [title, setTitle] = useState('제목입니다');
+  let [content, setContent] = useState('글내용입니다');
 
   return (
     <div className='m-10 card block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700'>
       <h5 className='mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
-        Card title
+        {title}
       </h5>
       <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
+        {content}
       </p>
       <button
         className='w-[5rem] h-10 rounded-lg text-base cursor-pointer mt-8 bg-blue text-white'
---
+++ fe/src/mockData/mockPosts.js
@@ -0,0 +1,10 @@
+const mockPosts = [
+    {
+        "title": "글 제목 입니다.",
+        "classificaion": "TECH",
+        "content": "글 내용입니다.",
+        "thumbnail": "https://github.com/JNU-econovation/econo-techblog-2.0/assets/88534959/22c3be37-52da-42f8-9b32-186b00bfaff8"
+    }
+];
+
+export default mockPosts;
