bacdfdef70d299052b06630cfbf97d4f39ee6946
Klomachenko <leekyumin0901@naver.com>
2023-10-03 15:02:26 +0900

    style : BoardList 스타일 수정

--- fe/src/components/pages/BoardList.jsx
+++ fe/src/components/pages/BoardList.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import MDEditor from '@uiw/react-md-editor';
+import React, { useState } from 'react';
 
 const BoardList = () => {
+  let [title, setTitle] = useState('');
+  let [content, setContent] = useState('');
+
   return (
-    <div className='grid place-items-center'>
-      <div className='flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:max-w-2xl md:flex-row'>
-        <img
-          className='h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
-          src='https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg'
-          alt=''
-        />
-        <div className='flex flex-col justify-start p-6'>
-          <h5 className='mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50'>
-            Card title
-          </h5>
-          <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </p>
-          <p className='text-xs text-neutral-500 dark:text-neutral-300'>
-            Last updated 3 mins ago
-          </p>
-        </div>
-      </div>
+    <div className='m-10 card block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700'>
+      <h5 className='mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
+        Card title
+      </h5>
+      <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <button
+        className='w-[5rem] h-10 rounded-lg text-base cursor-pointer mt-8 bg-blue text-white'
+        type='submit'
+      >
+        보러가기
+      </button>
     </div>
   );
 };
