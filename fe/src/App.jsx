65eb9bc371dc26653e6f87d3d238767457ec9aab
Klomachenko <leekyumin0901@naver.com>
2023-10-03 11:22:18 +0900

    feat : Board link 생성

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,24 +1,26 @@
-import "./index.css";
-import React from "react";
-import Header from "./components/common/Header";
-import LoginForm from "./components/login/LoginForm";
-import Main from "./components/main/Main";
-import SignUpForm from "./components/signup/SignUpForm";
-import Loading from "./components/common/Loading";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+import React from 'react';
+import Header from './components/common/Header';
+import LoginForm from './components/login/LoginForm';
+import Main from './components/main/Main';
+import SignUpForm from './components/signup/SignUpForm';
+import Loading from './components/common/Loading';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Board from './components/pages/Board';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <header>
           <Header />
         </header>
         <Routes>
-          <Route path="/" element={<Main />}></Route>
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/loading" element={<Loading />} />
+          <Route path='/' element={<Main />}></Route>
+          <Route path='/login' element={<LoginForm />} />
+          <Route path='/signup' element={<SignUpForm />} />
+          <Route path='/loading' element={<Loading />} />
+          <Route path='/board' element={<Board />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ fe/src/components/pages/Board.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Board = () => {
+    return <div>안녕하세요</div>;
+};
+
+export default Board;
