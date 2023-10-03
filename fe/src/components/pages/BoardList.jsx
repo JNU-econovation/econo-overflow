3b2f62590b6c752226472997f14922f6fc03514a
Klomachenko <leekyumin0901@naver.com>
2023-10-03 15:17:52 +0900

    feat : BoardList 구현

--- fe/src/components/pages/BoardList.jsx
+++ fe/src/components/pages/BoardList.jsx
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
+import mockPosts from '../../mockData/mockPosts';
 
 const BoardList = () => {
-  let [title, setTitle] = useState('제목입니다');
-  let [content, setContent] = useState('글내용입니다');
+  const [post, setPosts] = useState(mockPosts);
+  console.log(post[0].title);
 
   return (
-    <div className='m-10 card block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700'>
-      <h5 className='mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
-        {title}
-      </h5>
-      <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
-        {content}
-      </p>
-      <button
-        className='w-[5rem] h-10 rounded-lg text-base cursor-pointer mt-8 bg-blue text-white'
-        type='submit'
-      >
-        보러가기
-      </button>
+    <div>
+      {post.map((a, i) => {
+        console.log(a);
+        return (
+          <div>
+            <div className='m-10 card block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700'>
+              <h5 className='mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50'>
+                {post[i].title}
+              </h5>
+              <p className='mb-4 text-base text-neutral-600 dark:text-neutral-200'>
+                {post[i].content}
+              </p>
+              <button
+                className='w-[5rem] h-10 rounded-lg text-base cursor-pointer mt-8 bg-blue text-white'
+                type='submit'
+              >
+                보러가기
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- fe/src/mockData/mockPosts.js
+++ fe/src/mockData/mockPosts.js
@@ -1,8 +1,20 @@
 const mockPosts = [
     {
-        "title": "글 제목 입니다.",
+        "title": "글 제목1 입니다.",
         "classificaion": "TECH",
-        "content": "글 내용입니다.",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book..",
+        "thumbnail": "https://github.com/JNU-econovation/econo-techblog-2.0/assets/88534959/22c3be37-52da-42f8-9b32-186b00bfaff8"
+    },
+    {
+        "title": "글 제목2 입니다.",
+        "classificaion": "TECH",
+        "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
+        "thumbnail": "https://github.com/JNU-econovation/econo-techblog-2.0/assets/88534959/22c3be37-52da-42f8-9b32-186b00bfaff8"
+    },
+    {
+        "title": "글 제목3 입니다.",
+        "classificaion": "TECH",
+        "content": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.",
         "thumbnail": "https://github.com/JNU-econovation/econo-techblog-2.0/assets/88534959/22c3be37-52da-42f8-9b32-186b00bfaff8"
     }
 ];
