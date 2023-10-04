6e6710e98180eb5cc404d14f315febcee653ba54
capDoYeonLee <ghtyu21@naver.com>
2023-10-05 03:48:07 +0900

    fix : create database sql문 삭제

--- be/overflow/resources/develop-environment/mysql-init.d/00_init.sql
+++ be/overflow/resources/develop-environment/mysql-init.d/00_init.sql
@@ -8,6 +8,6 @@ GRANT ALL PRIVILEGES ON *.* TO
 GRANT ALL PRIVILEGES ON *.* TO
     'overflow-local'@'%';
 
-CREATE
-    DATABASE overflow DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CREATE
+--     DATABASE overflow DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
