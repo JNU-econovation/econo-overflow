b87377fd8311887a428d414f7a1c1424c32f6602
kssumin <201566@jnu.ac.kr>
2023-10-07 05:23:45 +0900

    fix : 별도의 클래스로 빼지 않고 saveUsecase의 private메서드에서 valid수행하도록 변경

--- be/overflow/src/main/java/com/econovation/overflow/auth/domain/usecase/SaveUserUseCase.java
+++ be/overflow/src/main/java/com/econovation/overflow/auth/domain/usecase/SaveUserUseCase.java
@@ -3,7 +3,6 @@ package com.econovation.overflow.auth.domain.usecase;
 import com.econovation.overflow.auth.domain.dto.request.SaveUserRequest;
 import com.econovation.overflow.auth.domain.model.UserModel;
 import com.econovation.overflow.auth.domain.model.converter.UserModelConverter;
-import com.econovation.overflow.auth.domain.service.ValidUserService;
 import com.econovation.overflow.auth.persistence.converter.UserEntityConverter;
 import com.econovation.overflow.auth.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +16,18 @@ public class SaveUserUseCase {
 	private final UserRepository userRepository;
 	private final UserModelConverter modelConverter;
 	private final UserEntityConverter entityConverter;
-	private final ValidUserService validUserService;
+	private final ValidEmailUseCase validEmailUseCase;
+	private final ValidNicknameUseCase validNicknameUseCase;
 
 	@Transactional
 	public void execute(SaveUserRequest request) {
 		UserModel userModel = modelConverter.from(request);
-		validUserService.execute(userModel);
+		valid(userModel);
 		userRepository.save(entityConverter.toEntity(userModel));
+	}
+
+	public void valid(UserModel userModel) {
+		validEmailUseCase.execute(userModel);
+		validNicknameUseCase.execute(userModel);
 	}
 }
