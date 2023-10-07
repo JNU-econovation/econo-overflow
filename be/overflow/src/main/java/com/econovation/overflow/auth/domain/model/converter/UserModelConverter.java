e0ddee851dd4a493fb3a446b69d703a0919fe2ac
kssumin <201566@jnu.ac.kr>
2023-10-08 04:45:43 +0900

    fix : jbcrypt로 암호화 구현하도록 변경

--- be/overflow/src/main/java/com/econovation/overflow/auth/domain/model/converter/UserModelConverter.java
+++ be/overflow/src/main/java/com/econovation/overflow/auth/domain/model/converter/UserModelConverter.java
@@ -1,23 +1,23 @@
 package com.econovation.overflow.auth.domain.model.converter;
 
 import com.econovation.overflow.auth.domain.dto.request.SaveUserRequest;
+import com.econovation.overflow.auth.domain.helper.Encoder;
 import com.econovation.overflow.auth.domain.model.UserModel;
 import com.econovation.overflow.common.support.converter.AbstractDtoConverter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class UserModelConverter implements AbstractDtoConverter<SaveUserRequest, UserModel> {
-	private final PasswordEncoder passwordEncoder;
+	private final Encoder encoder;
 
 	@Override
 	public UserModel from(SaveUserRequest request) {
 		return UserModel.builder()
 				.nickname(request.getNickname())
 				.email(request.getEmail())
-				.password(passwordEncoder.encode(request.getPassword()))
+				.password(encoder.encode(request.getPassword()))
 				.build();
 	}
 }
