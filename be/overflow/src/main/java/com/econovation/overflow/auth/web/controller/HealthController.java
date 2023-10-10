342bb84a857ba5e1d576e3acf3269d5585823cd6
kssumin <201566@jnu.ac.kr>
2023-10-10 10:35:32 +0900

    fix : restcontroller 어노테이션 추가

--- be/overflow/src/main/java/com/econovation/overflow/auth/web/controller/HealthController.java
+++ be/overflow/src/main/java/com/econovation/overflow/auth/web/controller/HealthController.java
@@ -7,7 +7,9 @@ import com.econovation.overflow.common.support.respnose.MessageCode;
 import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController {
 	@GetMapping("/api/health-check")
 	public ApiResponse<SuccessBody<String>> check() {
