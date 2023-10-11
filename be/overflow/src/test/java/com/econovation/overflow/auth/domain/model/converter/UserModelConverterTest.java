640f1e7d3177ab5bd93a47cd3c987b086d4b2936
kssumin <201566@jnu.ac.kr>
2023-10-11 20:24:57 +0900

    chore : lint 적용

--- be/overflow/src/test/java/com/econovation/overflow/auth/domain/model/converter/UserModelConverterTest.java
+++ be/overflow/src/test/java/com/econovation/overflow/auth/domain/model/converter/UserModelConverterTest.java
@@ -11,9 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 class UserModelConverterTest {
